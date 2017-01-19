fb827f1119d913e448af097a7fb797321b61c451
Alexej Haak <alexej.haak@outlook.de>
2017-01-19 11:09:39 +0100

    create database if necessary

--- codegen/src/main/scala/app/Main.scala
+++ codegen/src/main/scala/app/Main.scala
@@ -1,9 +1,12 @@
 package app
 
 import java.io.{File, PrintWriter}
+import java.sql.{DriverManager, Statement}
+
 import bay.driver.CustomizedPgDriver
 import com.typesafe.config.ConfigFactory
 import org.flywaydb.core.Flyway
+
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.io.Source
@@ -17,6 +20,7 @@ object Main extends App {
   println("Using Configuration File: " + configFile.getAbsolutePath)
   val config = ConfigFactory.parseFile(configFile).resolve()
 
+  println("Starting codegen")
   for {
     dbConfig <- config.getObject("slick.dbs")
   } yield {
@@ -31,34 +35,58 @@ object Main extends App {
     val excluded = List("schema_version") ++ config.getStringList(s"slick.dbs.$short.db.exclude")
 
     val profile = CustomizedPgDriver
-    val db      = CustomizedPgDriver.api.Database.forURL(url, driver = "org.postgresql.Driver", user = user, password = password)
+    val db = CustomizedPgDriver.api.Database.forURL(url,
+      driver = "org.postgresql.Driver",
+      user = user,
+      password = password)
+
+    println("Creating Database if necessary")
+    val c         = DriverManager.getConnection(url.reverse.dropWhile(_ != '/').reverse, user, password)
+    val statement = c.createStatement()
+    try {
+      statement.executeUpdate(s"CREATE DATABASE ${url.reverse.takeWhile(_ != '/').reverse};")
+    } catch {
+      case scala.util.control.NonFatal(e) =>
+    } finally {
+      statement.close()
+      c.close()
+    }
 
     println("Migrating using flyway..")
     val flyway = new Flyway
     flyway.setDataSource(url, user, password)
-    flyway.setValidateOnMigrate(false)  // Creates problems with windows machines
+    flyway.setValidateOnMigrate(false) // Creates problems with windows machines
     flyway.setLocations(s"filesystem:server/conf/db/migrations/$short")
     flyway.migrate()
 
     println("Starting codegen..")
-
     def sourceGen =
-      db.run(profile.createModel(Option(profile.defaultTables.map(ts => ts.filterNot(t => excluded contains t.name.name))))) map { model =>
+      db.run(profile.createModel(Option(profile.defaultTables.map(ts =>
+        ts.filterNot(t => excluded contains t.name.name))))) map { model =>
         new CustomizedCodeGenerator(model)
       }
 
     Await.ready(
-      sourceGen.map(codegen => codegen.writeToFile("bay.driver.CustomizedPgDriver", "dbdriver/src/main/scala", "models.auto_generated.slick", name, s"$name.scala")) recover {
+      sourceGen.map(
+        codegen =>
+          codegen.writeToFile("bay.driver.CustomizedPgDriver",
+            "dbdriver/src/main/scala",
+            "models.auto_generated.slick",
+            name,
+            s"$name.scala")) recover {
         case e: Throwable => e.printStackTrace()
       },
-      Duration.Inf)
+      Duration.Inf
+    )
 
-    val createdFile  = new File(s"dbdriver/src/main/scala/models/auto_generated/slick/$name.scala")
+    val createdFile = new File(s"dbdriver/src/main/scala/models/auto_generated/slick/$name.scala")
     createdFile.getParentFile.mkdirs
-    val modelSource  = Source.fromFile(createdFile).mkString
-    val sharedSource = modelSource.split("\n").map(_.trim).filter(_.startsWith("case class")).mkString("\n  ")
+    val modelSource = Source.fromFile(createdFile).mkString
+    val sharedSource =
+      modelSource.split("\n").map(_.trim).filter(_.startsWith("case class")).mkString("\n  ")
 
-    val filteredSource = modelSource.split("\n").filterNot(_.trim.startsWith("case class")).mkString("\n")
+    val filteredSource =
+      modelSource.split("\n").filterNot(_.trim.startsWith("case class")).mkString("\n")
 
     val sharedTemplate = s"""
                             |package shared.models.auto_generated
