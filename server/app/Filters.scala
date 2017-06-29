cddcaa2a21a336cd1b7c536ec8b5a698af8f2b79
Alexej Haak <alexej@recordbay.de>
2017-06-29 15:32:31 +0200

    remove not needed class

--- server/app/Filters.scala
+++
@@ -1,6 +0,0 @@
-import play.api.http.DefaultHttpFilters
-import play.filters.cors.CORSFilter
-import play.filters.gzip.GzipFilter
-
-class Filters(corsFilter: CORSFilter, gzipFilter: GzipFilter)
-    extends DefaultHttpFilters(corsFilter, gzipFilter)
